6fee563c1f23436f1e7524c87d987ee3cb8f16f2
antoniolazarosantos <antoniolazarosantos@gmail.com>
2022-11-23 17:57:05 -0300

    CSS com React

---
+++ arquivos/1_INTRO/4_CSS_REACT/css/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import MyComponent from './components/MyComponent';
+
+function App() {
+  return (
+    <div className="App">
+      {/* CSS Global */}
+      <h1> React com CSS</h1>
+        {/* CSS de Componente */}
+      <MyComponent/>
+      <p>Este parágrafo é do App.js</p>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ arquivos/1_INTRO/4_CSS_REACT/css/src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import './MyComponent.css';
+const MyComponent = () => {
+  return (
+    <div>
+    <p>MyComponent</p>
+    <h1>CSS do componente</h1>
+    <p>Mais um parágrafo</p>
+    <p className="my-comp-paragraph">Mais um parágrafo do componente</p>
+    </div>
+  )
+};
+
+export default MyComponent;
