f3deabb2f5abb213e355426f96319c9f5c3e9ab2
antoniolazarosantos <antoniolazarosantos@gmail.com>
2022-11-25 17:11:49 -0300

    Classes dinâmicas

--- arquivos/1_INTRO/4_CSS_REACT/css/src/App.js
+++ arquivos/1_INTRO/4_CSS_REACT/css/src/App.js
@@ -1,15 +1,30 @@
 import './App.css';
+import {useState} from "react";
 import MyComponent from './components/MyComponent';
 
 function App() {
+  const n = 7;
+  const [redTitle,setredTitle] = useState(false);
+
+  const handleClasseDinamica = () => {
+    setredTitle(!redTitle);
+  };
   return (
     <div className="App">
       {/* CSS Global */}
       <h1> React com CSS</h1>
-        {/* CSS de Componente */}
+      {/* CSS de Componente */}
       <MyComponent/>
       <p>Este parágrafo é do App.js</p>
-      
+      {/* Inline CSS */}
+      <p style={{color:"blue", padding:"25px", borderTop:"2px solid red"}}>
+        Este elemento foi estilizado de forma inline
+      </p>
+      {/*Css Inline dinâmico */}
+      <h2 style={n < 10 ? ({color:"purple"}):({color:"green"})}> Css dinâmico </h2>
+      {/*Classe dinâmica */}
+      <h2 className={redTitle ? "red-title":"title"}>Classe dinâmica</h2>
+      <button onClick={handleClasseDinamica}>Clique aqui para trocar a classe dinâmica</button>
     </div>
   );
 }
