3e1f4b20889f5df56dac31d7a23e98af4a9fe360
antoniolazarosantos <antoniolazarosantos@gmail.com>
2022-12-21 18:11:13 -0300

    Refatorando o contexto para Hook

--- arquivos/9_CONTEXT/context/src/pages/Home.js
+++ arquivos/9_CONTEXT/context/src/pages/Home.js
@@ -1,9 +1,13 @@
-import { useContext } from "react";
+//import { useContext } from "react";
 import ChangeCounter from "./../components/ChangeCounter";
-import { CounterContext } from "../context/CounterContext";
+//import { CounterContext } from "../context/CounterContext";
+
+// Refatorando hook
+import { useCounterContext } from "../hooks/useCounterContext";
 
 const Home = () => {
-    const { counter } = useContext(CounterContext);
+    //const { counter } = useContext(CounterContext);
+    const { counter } = useCounterContext();
   return (
     <div>
         <h1>Home</h1>
