833c64d90c8d9555919d93e5bc01d6460f8e3780
antoniolazarosantos <antoniolazarosantos@gmail.com>
2022-12-28 17:30:33 -0300

    Criando os componentes do projeto

--- arquivos/6_SECRET_WORD/secretword/src/App.js
+++ arquivos/6_SECRET_WORD/secretword/src/App.js
@@ -1,10 +1,38 @@
+//Css
 import './App.css';
+
+//React
+import {useCallback, useEffect, useState} from "react";
+
+//Dados
+import {wordsList} from "./data/words";
+
+// Componentes
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+
+const START = 0;
+const GAME = 1;
+const END = 2;
+const stages = [
+  {id:1,name:"start"},
+  {id:2,name:"game"},
+  {id:3,name:"end"},
+];
+
 
 function App() {
+  const [gameStage,setgameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+  console.log(words);
+
   return (
     <div className="App">
-      <StartScreen/>
+      {gameStage === stages[START].name && <StartScreen/> }
+      {gameStage === stages[GAME].name && <Game /> }
+      {gameStage === stages[END].name && <GameOver/> }
     </div>
   );
 }
