3fecbb3f80c6e06bc56d1259b0d88741ad84c733
antoniolazarosantos <antoniolazarosantos@gmail.com>
2023-01-05 17:04:25 -0300

    Resgatando dados da API ( com modificações )

--- arquivos/7_REQ_HTTP/httpreact/src/App.js
+++ arquivos/7_REQ_HTTP/httpreact/src/App.js
@@ -1,6 +1,24 @@
 import './App.css';
+import {useState, useEffect} from "react";
+
+
+const URL_BASE = "http://localhost:3000/products";
 
 function App() {
+  const [products,setProducts] = useState([]);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect (() => {
+    async function fetchData() {
+      const res = await fetch(URL_BASE);
+      const data = await res.json();
+      setProducts(data);
+    }
+    fetchData();    
+  },[]);
+
+  console.log(products);
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
