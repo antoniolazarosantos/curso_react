2060ccf41e98cbfcf3f3d889f57afcb26ca8f901
antoniolazarosantos <antoniolazarosantos@gmail.com>
2023-01-05 17:40:38 -0300

    Adicionando dados com React

--- arquivos/7_REQ_HTTP/httpreact/src/App.js
+++ arquivos/7_REQ_HTTP/httpreact/src/App.js
@@ -6,6 +6,8 @@ const URL_BASE = "http://localhost:3000/products";
 
 function App() {
   const [products,setProducts] = useState([]);
+  const [name,setName] = useState("");
+  const [price,setPrice] = useState("");
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect (() => {
@@ -17,6 +19,19 @@ function App() {
     fetchData();    
   },[]);
 
+  // Adicionando dados ao banco de dados
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Quando os states possuem os mesmos nomes das chaves, não precisa atribuir.
+    const product = {name,price}; 
+    const res = await fetch(URL_BASE,{
+      method:"POST",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify(product)
+  });
+};
 
   return (
     <div className="App">
@@ -26,6 +41,19 @@ function App() {
           <li key={product.id}>{product.name} - R$: {product.price}</li>
         ))}
       </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input type="text" name="name" value={name} onChange={(e)=> setName(e.target.value)} />
+          </label>
+          <label>
+            Valor:
+            <input type="number" name="price" value={price} onChange={(e)=> setPrice(e.target.value)} />
+          </label>
+          <input type="submit" value="Criar"/>
+        </form>
+      </div>
     </div>
   );
 }
